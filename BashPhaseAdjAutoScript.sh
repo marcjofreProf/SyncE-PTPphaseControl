f288474ecd907b46d1b4010966a699edd579cf33
Marc Jofre <marc.jofre@upc.edu>
2026-04-25 10:40:29 +0000

    Fix 5

--- BashPhaseAdjAutoScript.sh
+++ BashPhaseAdjAutoScript.sh
@@ -24,14 +24,14 @@ fi
 PlotInfo=true       # Set to true to see the PID math
 INTERFACE="eth0"
 N=150                # Number of samples to collect per interval
-TRIM_COUNT=4        # Trim average: Discard this many highest and lowest samples (e.g., 3 removes top 3 and bottom 3)
+TRIM_COUNT=4         # Trim average: Discard this many highest and lowest samples (e.g., 3 removes top 3 and bottom 3)
 psCLK_OUTperiod=$(( 4000 * 10 ))      # Period of the CLK_OUT signal in picoseconds
 psCLK_OUTperiodHalf=$((psCLK_OUTperiod/2))
 
 # --- PI Controller Tuning ---
 scaled_PID_factor=1000      # Scaling value to operate with integers
-scaled_PIDp=150             # Proportional gain (0.15)
-scaled_PIDi=120              # Integral gain (0.09)
+scaled_PIDp=100             # Proportional gain (0.10)
+scaled_PIDi=45              # Integral gain (0.045)
 
 # --- Initialize Persistent Memory ---
 # Because the script stays alive, this variable simply persists in RAM!
